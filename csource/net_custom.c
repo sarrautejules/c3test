e81ae36a263c2a60502a4b39222a0f80cc53cae0
Jules Sarraute <jules.sarraute@gmail.com>
2025-01-08 22:54:57 +0100

    refactor

--- csource/net_custom.c
+++ csource/net_custom.c
@@ -7,19 +7,16 @@
 #include <string.h>
 
 void get_ip_from_hostname(char* hostname, char* resolved_ip) {
-
-  struct sockaddr_in *addr;
-  struct addrinfo hints;
-  memset(&hints, 0, sizeof(hints));
-  char ip[INET_ADDRSTRLEN];
-  hints.ai_family = AF_INET; 
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_protocol = IPPROTO_TCP;
-  struct addrinfo *result;
-  int status = getaddrinfo("google.fr", "80", &hints, &result);
-  addr = (struct sockaddr_in*)result->ai_addr;
-  inet_ntop(AF_INET, &addr->sin_addr, resolved_ip, INET_ADDRSTRLEN);
-//   memcpy(resolved_ip, ip, sizeof(ip));
-  freeaddrinfo(result);
-
+    struct sockaddr_in *addr;
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    char ip[INET_ADDRSTRLEN];
+    hints.ai_family = AF_INET; 
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    struct addrinfo *result;
+    int status = getaddrinfo("google.fr", "80", &hints, &result);
+    addr = (struct sockaddr_in*)result->ai_addr;
+    inet_ntop(AF_INET, &addr->sin_addr, resolved_ip, INET_ADDRSTRLEN);
+    freeaddrinfo(result);
 }
